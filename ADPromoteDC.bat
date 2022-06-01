8d93f5b544f49e6701fb1d40d43896bb975240ca
Ryan Roberts <rroberts@ssiusa.com>
2022-05-31 20:06:38 -1000

    batch

---
+++ ADPromoteDC.bat
@@ -0,0 +1,2 @@
+# Promote to DC
+dcpromo.exe /unattend /replicaornewdomain:Replica /replicadomaindnsname:lucernpub.com /userdomain:LUCERNPUB /username:administrator /password:"P@$$w0rd" /sitename:RemoteLocation /installdns:yes /confirmgc:yes /databasepath:"E:\NTDS" /logpath:"E:\logs" /sysvolpath:"E:\sysvol" /safemodeadminpassword:"P@$$w0rd”

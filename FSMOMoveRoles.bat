54f4b8a72d2c45f7533c0923a3e64ee5a23796b7
Ryan Roberts <rroberts@ssiusa.com>
2022-05-31 23:27:11 -1000

    batch

--- FSMOMoveRoles.bat
+++ FSMOMoveRoles.bat
@@ -1,2 +1,2 @@
 :: Move FSMO Roles
-ntdsutil "roles" "connections" "connect to server New-DC" "q" "transfer schema master" "transfer naming master" "transfer PDC" "transfer RID master" "transfer infrastructure master" "q" "q"
+ntdsutil "roles" "connections" "connect to server New-DC" "q" "transfer schema master" "transfer naming master" "transfer PDC" "transfer RID master" "transfer infrastructure master" q q
--- FSMOSeizeRoles.bat
+++ FSMOSeizeRoles.bat
@@ -1,2 +1,2 @@
 :: Seize FSMO Roles
-ntdsutil "roles" "connections" "connect to server Old-DC" "q" "seize schema master" "seize naming master" "seize PDC" "seize RID master" "seize infrastructure master" "q" "q"
+ntdsutil "roles" "connections" "connect to server Old-DC" "q" "seize schema master" "seize naming master" "seize PDC" "seize RID master" "seize infrastructure master" q q

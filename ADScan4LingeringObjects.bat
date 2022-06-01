6f740cd8f8ebf14e18b995ac5da29af4bdb4b8ff
Ryan Roberts <rroberts@ssiusa.com>
2022-05-31 22:55:27 -1000

    batch

---
+++ ADScan4LingeringObjects.bat
@@ -0,0 +1,4 @@
+:: Find the objectGUID attribute of the domain controller. Use the following command on an elevated Command Prompt:
+dsquery.exe * "CN=DC01,OU=Domain Controllers,DC=LucernPub,DC=com" -scope base -attr objectguid
+:: Scan for lingering objects:
+repadmin.exe /removelingeringobjects <FQDN of Domain Controller with suspected lingering objects> <objectGUID of Domain Controller with correct data> <Distinguished Name of partition containing lingering objects> /advisory_mode 
